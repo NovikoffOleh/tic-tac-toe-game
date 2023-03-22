a05ecfa77817a5c3c578e215ca877fd06b818800
NovikoffOleh <novikoff10000@gmail.com>
2023-03-22 19:37:09 +0200

    Update Game.jsx

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Board from './Board';
+import { calculateWinner } from "../helper";
 import  './Game.css';
 
-const Game =() => {
+const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+  const winner = calculateWinner(board)
+
+  const handleClick = (index) => {
+    const boardCopy = [...board]
+    //определить біл ли клик по ячейке
+
+    //определить чей ход
+
+    //обновить стейт
+  }
+
   return (
     <div className='wrapper'>
-      <Board/>
+      <Board squares={ board} />
     </div>
   )
 }
